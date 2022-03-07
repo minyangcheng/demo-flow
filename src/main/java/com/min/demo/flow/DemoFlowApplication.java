cd829a23abcd2785473312b9e77f940371b4b9d9
mengqi <mengqi.min@tuya.com>
2022-03-07 17:56:20 +0800

    init

---
+++ src/main/java/com/min/demo/flow/DemoFlowApplication.java
@@ -0,0 +1,13 @@
+package com.min.demo.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/min/demo/flow/DemoFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.demo.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
